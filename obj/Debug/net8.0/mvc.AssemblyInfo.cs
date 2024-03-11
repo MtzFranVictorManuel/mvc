457c596db3d328f1031426a46ef608a381d6ab9d
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-03-10 22:48:04 -0600

    Actividad mvc

--- obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e382440b9ab86393211f5833004701fc70df5bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d83af79c605075aab57cc5763a0100ba366d55b")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
